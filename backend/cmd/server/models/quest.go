fc3209f8895c1526366f01c86fe0fb0323031749
mavericklim <128111928+mavericklim@users.noreply.github.com>
2025-05-15 21:12:12 +0800

    Update variable/variable names

--- backend/cmd/server/models/quest.go
+++ backend/cmd/server/models/quest.go
@@ -2,8 +2,7 @@ package models
 
 type Quest struct {
     ID         int       `json:"id"`
-    UserID     string    `json:"user_id"`
     Title      string    `json:"title"`
-    Completed  bool      `json:"completed"`
-    ExpReward  int       `json:"exp_reward"`
+    Description  string      `json:"description"`
+    Completed  bool       `json: "completed"`
 }
