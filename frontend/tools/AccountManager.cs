d979a6bb29823d2852ebfd06114a89e0dcdfc919
andrewsoonqn <andrewsoonqn@gmail.com>
2025-05-29 16:04:26 +0800

    Create AccountManager.cs
    Stores basic username info

---
+++ frontend/tools/AccountManager.cs
@@ -0,0 +1,16 @@
+using Godot
+
+public partial class AccountManager : Node
+{
+    private string Username {get; set;}
+
+    public void SetUsername(string username)
+    {
+        this.Username = username;
+    }
+
+    public string GetUsername()
+    {
+        return this.Username;
+    }
+}
