103ccf614e42484e5c7eece47fdf33779e7baf6f
andrewsoonqn <andrewsoonqn@gmail.com>
2025-05-29 17:16:07 +0800

    Implement time selection page

--- frontend/passive/Passive.cs
+++ frontend/passive/Passive.cs
@@ -6,30 +6,21 @@ public partial class Passive : Control
     [Export]
     private Button _backToHomeButton;
 
-    [Export]
-    private Button _addExpButton;
-
-    [Export]
-    private Button _resetExpButton;
-
-    private StatsManager _statsManager;
+    [Export] private Label _timeInfoLabel;
+    [Export] private Slider _timeSlider;
+    [Export] private Button _startButton;
 
     public override void _Ready()
     {
+        _timeSlider.Value = 20;
+        _timeInfoLabel.Text = $"Enter dungeon for: {_timeSlider.Value} seconds";
         _backToHomeButton.Pressed += BackToHomeButtonOnPressed;
-        _addExpButton.Pressed += AddExpButtonOnPressed;
-        _resetExpButton.Pressed += ResetExpButtonOnPressed;
-        _statsManager = GetNode<StatsManager>("/root/StatsManager");
+        _timeSlider.ValueChanged += TimeSliderOnValueChanged;
     }
 
-    private void AddExpButtonOnPressed()
+    private void TimeSliderOnValueChanged(double value)
     {
-        _statsManager.AddExp(50);
-    }
-
-    private void ResetExpButtonOnPressed()
-    {
-        _statsManager.ResetExp();
+        _timeInfoLabel.Text = $"Enter dungeon for: {value} seconds";
     }
 
     private void BackToHomeButtonOnPressed()
