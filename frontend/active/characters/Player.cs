e4707cb73d8bed065cd73dc977c388742c6758cb
andrewsoonqn <andrewsoonqn@gmail.com>
2025-05-29 21:49:42 +0800

    Add weapon with mouse following

--- frontend/active/characters/Player.cs
+++ frontend/active/characters/Player.cs
@@ -3,40 +3,58 @@ using System;
 
 public partial class Player : Character
 {
+    private Node2D _sword;
+    public override void _Ready()
+    {
+        base._Ready();
+        _sword = this.GetNode<Node2D>("Sword");
+    }
+
     public override void _Process(double delta)
     {
-        // Vector2 mouseDirection = (GetGlobalMousePosition() - GlobalPosition).Normalized();
+        Vector2 mouseDirection = (GetGlobalMousePosition() - GlobalPosition).Normalized();
 
-        // if (mouseDirection.X > 0 && _animatedSprite.FlipH)
-        if (_movDirection.X > 0 && _animatedSprite.FlipH)
+        if (mouseDirection.X > 0 && AnimatedSprite.FlipH)
+        // if (MovDirection.X > 0 && AnimatedSprite.FlipH)
         {
-            _animatedSprite.FlipH = false;
-            // } else if (mouseDirection.X < 0 && !_animatedSprite.FlipH)
+            AnimatedSprite.FlipH = false;
+
         }
-        else if (_movDirection.X < 0 && !_animatedSprite.FlipH)
+        // else if (MovDirection.X < 0 && !AnimatedSprite.FlipH)
+        else if (mouseDirection.X < 0 && !AnimatedSprite.FlipH)
         {
-            _animatedSprite.FlipH = true;
+            AnimatedSprite.FlipH = true;
+        }
+
+        _sword.Rotation = mouseDirection.Angle();
+        if (mouseDirection.X < 0 && _sword.Scale.Y > 0)
+        {
+            _sword.ApplyScale(new Vector2(1, -1));
+        }
+        else if (mouseDirection.X > 0 && _sword.Scale.Y < 0)
+        {
+            _sword.ApplyScale(new Vector2(1, -1));
         }
     }
 
     public override void GetInput()
     {
-        _movDirection = Vector2.Zero;
+        MovDirection = Vector2.Zero;
         if (Input.IsActionPressed("ui_up") || Input.IsKeyPressed(Key.W))
         {
-            _movDirection += Vector2.Up;
+            MovDirection += Vector2.Up;
         }
         else if (Input.IsActionPressed("ui_down") || Input.IsKeyPressed(Key.S))
         {
-            _movDirection += Vector2.Down;
+            MovDirection += Vector2.Down;
         }
         else if (Input.IsActionPressed("ui_left") || Input.IsKeyPressed(Key.A))
         {
-            _movDirection += Vector2.Left;
+            MovDirection += Vector2.Left;
         }
         else if (Input.IsActionPressed("ui_right") || Input.IsKeyPressed(Key.D))
         {
-            _movDirection += Vector2.Right;
+            MovDirection += Vector2.Right;
         }
     }
 }
