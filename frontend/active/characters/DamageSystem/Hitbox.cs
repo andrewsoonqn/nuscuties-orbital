ebd579ae0b6ef5727188b43347e36b202eaac8e9
andrewsoonqn <andrewsoonqn@gmail.com>
2025-06-30 09:47:48 +0800

    Integrate DerivedStatCalculator for player damage
    Refactored Hitbox to use DerivedStatCalculator for calculating player attack damage, enabling stat-based scaling. Updated DerivedStatCalculator to be a Godot Node and fetch player stats and progression internally. Also made health and state labels visible in game_world.tscn and registered DerivedStatCalculator as an autoload singleton in project.godot.

--- frontend/active/characters/DamageSystem/Hitbox.cs
+++ frontend/active/characters/DamageSystem/Hitbox.cs
@@ -1,5 +1,6 @@
 using Godot;
 using nuscutiesapp.active.characters.DamageSystem;
+using nuscutiesapp.tools;
 using System;
 
 public partial class Hitbox : Area2D
@@ -10,12 +11,16 @@ public partial class Hitbox : Area2D
     private Vector2 knockbackDirection;
     [Export] private float knockbackMagnitude;
 
+    private DerivedStatCalculator _derivedStatCalculator;
+
     private CollisionShape2D _collisionShape;
 
     public override void _Ready()
     {
         this._collisionShape = this.GetNode<CollisionShape2D>("CollisionShape2D");
         this.BodyEntered += OnBodyEntered;
+        
+        _derivedStatCalculator = this.GetNode<DerivedStatCalculator>("/root/DerivedStatCalculator");
     }
 
     private void OnBodyEntered(Node2D body)
@@ -24,10 +29,22 @@ public partial class Hitbox : Area2D
         {
             this.knockbackDirection = character.GlobalPosition - Wielder.GlobalPosition; // TODO change this
             knockbackDirection = knockbackDirection.Normalized();
-            DamageInfo damage = new DamageInfo(
-                this.damage, knockbackDirection * knockbackMagnitude
+            
+            // TODO use a pattern here
+            float damageAmt;
+            if (Wielder is Player)
+            {
+                damageAmt = this.damage * _derivedStatCalculator.CalcAttackDamageMultiplier();
+            }
+            else
+            {
+                damageAmt = this.damage;
+            }
+            
+            DamageInfo damageInfo = new DamageInfo(
+                damageAmt, knockbackDirection * knockbackMagnitude
             );
-            character.TakeDamage(damage);
+            character.TakeDamage(damageInfo);
         }
     }
 }
--- frontend/tools/DerivedStatCalculator.cs
+++ frontend/tools/DerivedStatCalculator.cs
@@ -1,32 +1,43 @@
+using Godot;
+
 namespace nuscutiesapp.tools
 {
-    public class DerivedStatCalculator
+    public partial class DerivedStatCalculator : Node
     {
-        private const double _strBonusPerPoint = 0.03;
+        private const float _strBonusPerPoint = 0.03f;
         private const int _staReductionPerPoint = 2;
         private const int _HPIncreasePerLevel = 10;
         private const int _baseHP = 60;
         private const int _damageIncreasePerLevel = 5;
         private const int _baseDamage = 20;
+        
+        private ProgressionManager _progressionManager;
+        private PlayerStatManager _playerStatManager;
 
-        public double CalcAttackDamageMultiplier(int strength)
+        public override void _Ready()
         {
-            return 1 + strength * _strBonusPerPoint;
+            _playerStatManager = GetNode<PlayerStatManager>("/root/PlayerStatManager");
+            _progressionManager = GetNode<ProgressionManager>("/root/ProgressionManager");
         }
 
-        public double CalcDamageReduction(int stamina)
+        public float CalcAttackDamageMultiplier()
         {
-            return stamina * _staReductionPerPoint;
+            return 1 + _playerStatManager.GetStrength() * _strBonusPerPoint;
         }
 
-        public double CalcMaxHP(int level)
+        public double CalcDamageReduction()
         {
-            return _baseHP + (level * _HPIncreasePerLevel);
+            return _playerStatManager.GetStamina() * _staReductionPerPoint;
         }
 
-        public double CalcDamage(int level)
+        public double CalcMaxHP()
         {
-            return _baseDamage + (level * _damageIncreasePerLevel);
+            return _baseHP + (_progressionManager.GetLevel() * _HPIncreasePerLevel);
+        }
+
+        public double CalcDamage()
+        {
+            return _baseDamage + (_progressionManager.GetLevel() * _damageIncreasePerLevel);
         }
     }
 }
