8d327fba7479d3f4f40088f609173f153fd3fd81
andrewsoonqn <andrewsoonqn@gmail.com>
2025-07-14 19:35:46 +0800

    Fix wave information to reset every game

--- frontend/active/WaveInformation.cs
+++ frontend/active/WaveInformation.cs
@@ -18,10 +18,9 @@ public partial class WaveInformation : Node
     {
         _eventManager = GetNode<ActiveDungeonEventManager>("/root/ActiveDungeonEventManager");
 
-        _totalWaves = GetParent().GetNode<ActiveGame>("GameWorld").GetMaxWaves();
-        _wavesLabel.Text = $"Wave 0/{_totalWaves}";
-        _enemiesSpawnedLabel.Text = $"Enemies Spawning...";
-
+        InitializeWaveInformation();
+        
+        _eventManager.GameStartedEvent += InitializeWaveInformation;
         _eventManager.EnemySpawnedEvent += OnEnemySpawned;
         _eventManager.WaveElapsedEvent += OnWaveElapsed;
         _eventManager.EnemyDiedEvent += OnEnemyDied;
@@ -29,6 +28,12 @@ public partial class WaveInformation : Node
         base._Ready();
     }
 
+    public void InitializeWaveInformation()
+    {
+        _totalWaves = GetParent().GetNode<ActiveGame>("GameWorld").GetMaxWaves();
+        _wavesLabel.Text = $"Wave 0/{_totalWaves}";
+        _enemiesSpawnedLabel.Text = $"Enemies Spawning...";
+    }
     public void OnEnemyDied()
     {
         _enemiesRemaining--;
