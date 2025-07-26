c30a237f8fe9d7e58361e2f86b8f9dd88a1dabf0
mavericklim <mavericklimqixun@gmail.com>
2025-07-26 23:03:19 +0800

    Update Passive Dungeon UI and minor tweaks to logic
    Update passive dungeon UI and change from seconds to minutes
    
    Change HSlider minimum to 1 minute
    
    Rewards accumulated updates dynamically
    
    Add coins as rewards
    
    Change passive end scene from just seconds to minutes and seconds

--- frontend/passive/PassiveEndScene.cs
+++ frontend/passive/PassiveEndScene.cs
@@ -3,41 +3,46 @@ using nuscutiesapp.tools;
 
 public partial class PassiveEndScene : Node
 {
-    [Export]
-    private Label _expAccumulatedLabel;
+	[Export]
+	private Label _expAccumulatedLabel;
 
-    [Export]
-    private Label _coinsAccumulatedLabel;
+	[Export]
+	private Label _coinsAccumulatedLabel;
 
-    [Export]
-    private Label _totalTimeSpentLabel;
+	[Export]
+	private Label _totalTimeSpentLabel;
 
-    [Export]
-    private Button _returnButton;
+	[Export]
+	private Button _returnButton;
 
-    private PassiveSessionInfoManager _passiveSessionInfoManager;
-    private ProgressionManager _expManager;
-    private PlayerInventoryManager _inventoryManager;
+	private PassiveSessionInfoManager _passiveSessionInfoManager;
+	private ProgressionManager _expManager;
+	private PlayerInventoryManager _inventoryManager;
 
-    public override void _Ready()
-    {
-        this._passiveSessionInfoManager = this.GetNode<PassiveSessionInfoManager>("/root/PassiveSessionInfoManager");
-        this._expManager = this.GetNode<ProgressionManager>("/root/ProgressionManager");
-        this._inventoryManager = this.GetNode<PlayerInventoryManager>("/root/PlayerInventoryManager");
+	public override void _Ready()
+	{
+		this._passiveSessionInfoManager = this.GetNode<PassiveSessionInfoManager>("/root/PassiveSessionInfoManager");
+		this._expManager = this.GetNode<ProgressionManager>("/root/ProgressionManager");
+		this._inventoryManager = this.GetNode<PlayerInventoryManager>("/root/PlayerInventoryManager");
 
-        int expGained = _passiveSessionInfoManager.getAccumulatedExp();
-        int coinsGained = _passiveSessionInfoManager.getAccumulatedCoins();
+		int expGained = _passiveSessionInfoManager.getAccumulatedExp();
+		int coinsGained = _passiveSessionInfoManager.getAccumulatedCoins();
 
-        _expManager.AddExp(expGained);
-        _inventoryManager.AddCoins(coinsGained);
+		_expManager.AddExp(expGained);
+		_inventoryManager.AddCoins(coinsGained);
 
-        this._expAccumulatedLabel.Text = $"Total EXP Gained: {expGained}";
-        if (_coinsAccumulatedLabel != null)
-        {
-            this._coinsAccumulatedLabel.Text = $"Total Coins Gained: {coinsGained}";
-        }
-        this._totalTimeSpentLabel.Text = $"Total Time Spent: {_passiveSessionInfoManager.getTimeSpent():F0} seconds";
+		this._expAccumulatedLabel.Text = $"Total EXP Gained: {expGained}";
+		if (_coinsAccumulatedLabel != null)
+		{
+			this._coinsAccumulatedLabel.Text = $"Total Coins Gained: {coinsGained}";
+		}
+		
+		double timeSpentSeconds = _passiveSessionInfoManager.getTimeSpent();
+		int minutes = (int)(timeSpentSeconds / 60);
+		int seconds = (int)(timeSpentSeconds % 60);
+		this._totalTimeSpentLabel.Text = $"Total Time Spent: {minutes:D2}:{seconds:D2}";
+	
 
-        this._returnButton.Pressed += () => this.GetTree().ChangeSceneToFile(Paths.Passive);
-    }
+		this._returnButton.Pressed += () => this.GetTree().ChangeSceneToFile(Paths.Passive);
+	}
 }
